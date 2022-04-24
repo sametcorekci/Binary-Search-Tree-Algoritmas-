0528d429aea1cf5fef2f5ada459178463f3befb9
sametcorekci <85850750+sametcorekci@users.noreply.github.com>
2022-04-25 02:58:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Binary_Serach_Algoriması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Proje 3
+            // [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+            // Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb.
+
+            //Binary Search Tree
+            //Bir düğüm her iki tarafa da referans verebiliyor. Sağ ve sol olarak. Sağ tarafından kendinden büyük elemanlar, sol tarafında ise kendinden küçük elemanlar bulunacak.
+        }
+    }
+}
